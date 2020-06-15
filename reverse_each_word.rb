20cd1826321db0c4b8c747670ed5c4edb0c443b5
Eve Reichmann <ereichmann4@aol.com>
2020-06-15 21:25:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ require 'pry'
 def reverse_each_word(sentence)
   #sentence.split.collect{|word| "#{word.reverse}"}.join(" #")
   
-  sentence.split.collect {|words| "#{words.reverse}"} 
+  sentence.split 
 
 end
